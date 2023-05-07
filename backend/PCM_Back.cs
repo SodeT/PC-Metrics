0bedbf1c851030522b5f73f5a4f46c1bf56e0219
SodeT <42680887+SodeT@users.noreply.github.com>
2023-05-08 01:43:32 +0200

    working prototype

---
+++ backend/PCM_Back.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+using Hardware.Info;
+
+static class PCM_Backend
+{
+    const int DATAPOINTS_PER_FILE = 10;
+    static readonly IHardwareInfo hardwareInfo = new HardwareInfo();
+
+    public static void Main()
+    {
+        DateTime lastLog = DateTime.Now;
+        int dataPointCount = 0;
+        float[] data = new float[DATAPOINTS_PER_FILE];
+
+        while (true)
+        {
+            if (lastLog.AddSeconds(5) < DateTime.Now)
+            {
+                hardwareInfo.RefreshMemoryStatus();
+                float memUsage = hardwareInfo.MemoryStatus.AvailablePhysical / (float)hardwareInfo.MemoryStatus.TotalPhysical;
+                data[dataPointCount] = 1 - memUsage;
+
+                Console.WriteLine(data[dataPointCount]);
+
+                lastLog = DateTime.Now;
+                dataPointCount++;
+
+                if (dataPointCount >= DATAPOINTS_PER_FILE)
+                {
+
+                    int fileCount = Directory.GetFiles("../data/", "*.pcmd", SearchOption.TopDirectoryOnly).Length;
+
+                    FileStream fstream = File.Open("../data/d" + fileCount.ToString() + ".pcmd", FileMode.Create);
+                    BinaryWriter writer = new BinaryWriter(fstream, Encoding.UTF8, false);
+
+                    for (int i = 0; i < DATAPOINTS_PER_FILE; i++)
+                    {
+                        writer.Write(data[i]);
+                    }
+
+                    writer.Close();
+                    dataPointCount = 0;
+                }
+            } 
+        }
+    }
+}
---
+++ frontend/PCM_Front.cs
@@ -0,0 +1,68 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+
+static class PCM_Frontend
+{
+    const int DATAPOINTS_PER_FILE = 10;
+    const int WIDTH = 800;
+    const int HEIGHT = 480;
+
+    
+    public static void Main()
+    {
+        Raylib.InitWindow(WIDTH, HEIGHT, "PC-Metrics");
+
+        float[] data = LoadData();
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int blockHeight = (int)(data[i] * HEIGHT);
+                int blockWidht = WIDTH / data.Length;
+                Raylib.DrawRectangle(i * blockWidht, HEIGHT- blockHeight, blockWidht, blockHeight, Color.WHITE);
+            }
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+
+    public static float[] LoadData()
+    {
+        int fileCount = Directory.GetFiles("../data/", "*.pcmd", SearchOption.TopDirectoryOnly).Length;
+        float[] data = new float[fileCount * DATAPOINTS_PER_FILE];
+        for (int i = 0; i < fileCount; i++)
+        {
+            ReadDataFile(i).CopyTo(data, i * DATAPOINTS_PER_FILE);
+        }
+
+        return data;
+    }
+
+    public static float[] ReadDataFile(int index)
+    {
+        float[] ret = new float[DATAPOINTS_PER_FILE];
+        string path = "../data/d" + index.ToString() + ".pcmd";
+
+        FileStream fstream = File.Open(path, FileMode.Open);
+        BinaryReader reader = new BinaryReader(fstream, Encoding.UTF8, false);
+        
+        for (int i = 0; i < DATAPOINTS_PER_FILE; i++)
+        { 
+            ret[i] = reader.ReadSingle();
+        } 
+        
+        reader.Close();
+
+        return ret;
+    }
+}
