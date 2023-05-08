0db5e24e795bc7ea7606139b5e8706c6009f060e
SodeT <42680887+SodeT@users.noreply.github.com>
2023-05-08 14:13:25 +0200

    nice UI

--- backend/PCM_Back.cs
+++ backend/PCM_Back.cs
@@ -6,37 +6,36 @@ using Hardware.Info;
 
 static class PCM_Backend
 {
-    const int DATAPOINTS_PER_FILE = 10;
+    const int DATA_PER_HOUR = 60;
+    const int TIME_BETWEEN_DATA = DATA_PER_HOUR / 3600;
     static readonly IHardwareInfo hardwareInfo = new HardwareInfo();
 
     public static void Main()
     {
         DateTime lastLog = DateTime.Now;
         int dataPointCount = 0;
-        float[] data = new float[DATAPOINTS_PER_FILE];
+        float[] data = new float[DATA_PER_HOUR];
 
         while (true)
         {
-            if (lastLog.AddSeconds(5) < DateTime.Now)
+            if (lastLog.AddSeconds(1) < DateTime.Now)
             {
                 hardwareInfo.RefreshMemoryStatus();
                 float memUsage = hardwareInfo.MemoryStatus.AvailablePhysical / (float)hardwareInfo.MemoryStatus.TotalPhysical;
                 data[dataPointCount] = 1 - memUsage;
 
-                Console.WriteLine(data[dataPointCount]);
-
                 lastLog = DateTime.Now;
                 dataPointCount++;
 
-                if (dataPointCount >= DATAPOINTS_PER_FILE)
+                if (dataPointCount >= DATA_PER_HOUR)
                 {
 
                     int fileCount = Directory.GetFiles("../data/", "*.pcmd", SearchOption.TopDirectoryOnly).Length;
 
-                    FileStream fstream = File.Open("../data/d" + fileCount.ToString() + ".pcmd", FileMode.Create);
+                    FileStream fstream = File.Open("../data/h" + fileCount.ToString() + ".pcmd", FileMode.Create);
                     BinaryWriter writer = new BinaryWriter(fstream, Encoding.UTF8, false);
 
-                    for (int i = 0; i < DATAPOINTS_PER_FILE; i++)
+                    for (int i = 0; i < DATA_PER_HOUR; i++)
                     {
                         writer.Write(data[i]);
                     }
--- frontend/PCM_Front.cs
+++ frontend/PCM_Front.cs
@@ -1,5 +1,7 @@
-﻿using Raylib_cs;
+﻿
+using Raylib_cs;
 using System;
+using System.Numerics;
 using System.Collections;
 using System.IO;
 using System.Text;
@@ -7,29 +9,100 @@ using System.Text;
 
 static class PCM_Frontend
 {
-    const int DATAPOINTS_PER_FILE = 10;
-    const int WIDTH = 800;
-    const int HEIGHT = 480;
+    const int DATA_PER_HOUR = 60;
+    const int WIDTH = 1600;
+    const int HEIGHT = 900;
+    const int BLOCK_WIDTH = 1;
 
-    
+    static Color graphColor = new Color(0, 255, 0, 255);
+    static Color fillGraphColor = new Color(0, 255, 0, 55);
+    static Color bgColor = new Color(41, 43, 44, 255);
+    static Color textColor = new Color(230, 230, 230, 255);
+    static Color buttonColor = new Color(65, 72, 96, 255);
+    static Color gridColor = new Color(255, 255, 0, 105);
+
     public static void Main()
     {
         Raylib.InitWindow(WIDTH, HEIGHT, "PC-Metrics");
 
         float[] data = LoadData();
+        float[] dataView = data;
+        int dataViewLenth = data.Length;
+
+        string[] buttonPresets = {"reload", "all", "1h", "6h", "12h", "24h"};
+        Button[] btns = SpawnButtons(buttonPresets, 5);
 
         while (!Raylib.WindowShouldClose())
         {
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.BLACK);
-
-            for (int i = 0; i < data.Length; i++)
+            if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_BUTTON_LEFT))
             {
-                int blockHeight = (int)(data[i] * HEIGHT);
-                int blockWidht = WIDTH / data.Length;
-                Raylib.DrawRectangle(i * blockWidht, HEIGHT- blockHeight, blockWidht, blockHeight, Color.WHITE);
+                Button btn = ButtonClicked(btns, Raylib.GetMousePosition());
+                if (btn != null)
+                {
+                    switch (btn.Text)
+                    {
+                    case "reload":
+                        if (dataViewLenth == data.Length)
+                        {
+                            data = LoadData();
+                            dataViewLenth = data.Length;
+                        }
+                        break;
+                    case "all":
+                        dataViewLenth = data.Length;
+                        break;
+                    case "1h":
+                        dataViewLenth = DATA_PER_HOUR;
+                        break;
+                    case "6h":
+                        dataViewLenth = DATA_PER_HOUR * 6;
+                        break;
+                    case "12h":
+                        dataViewLenth = DATA_PER_HOUR * 12;
+                        break;
+                    case "24h":
+                        dataViewLenth = DATA_PER_HOUR * 24;
+                        break;
+                    default:
+                        break;
+                    }
+
+                    dataViewLenth = (int)MathF.Min(data.Length, dataViewLenth);
+                    dataView = new float[dataViewLenth];
+                    int j = 0;
+                    for (int i = data.Length - dataViewLenth; i < data.Length; i++, j++)
+                    {
+                        dataView[j] = data[i];
+                    }
+                }
             }
 
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(bgColor);
+
+
+            int blockCount = WIDTH / BLOCK_WIDTH;
+            int prevx = 0;
+            int prevy = 0;
+            for (int i = 0; i < blockCount; i++)
+            {
+                int dataPoint = (int)(((float)i/blockCount) * dataView.Length);
+                int blockHeight = (int)(dataView[dataPoint] * HEIGHT);
+
+                Raylib.DrawRectangle(i * BLOCK_WIDTH, HEIGHT- blockHeight, BLOCK_WIDTH, blockHeight, fillGraphColor);
+                Raylib.DrawLine(prevx, prevy, i * BLOCK_WIDTH, HEIGHT - blockHeight, graphColor);
+                prevx = i * BLOCK_WIDTH;
+                prevy = HEIGHT - blockHeight;
+                
+            }
+
+            DrawGrid(3);
+
+            for (int i = 0; i < btns.Length; i++)
+            {
+			    btns[i].Draw();
+            }
             Raylib.EndDrawing();
         }
 
@@ -39,24 +112,25 @@ static class PCM_Frontend
     public static float[] LoadData()
     {
         int fileCount = Directory.GetFiles("../data/", "*.pcmd", SearchOption.TopDirectoryOnly).Length;
-        float[] data = new float[fileCount * DATAPOINTS_PER_FILE];
+        float[] data = new float[fileCount * DATA_PER_HOUR];
         for (int i = 0; i < fileCount; i++)
         {
-            ReadDataFile(i).CopyTo(data, i * DATAPOINTS_PER_FILE);
+            ReadDataFile(i).CopyTo(data, i * DATA_PER_HOUR);
         }
 
+     
         return data;
     }
 
     public static float[] ReadDataFile(int index)
     {
-        float[] ret = new float[DATAPOINTS_PER_FILE];
-        string path = "../data/d" + index.ToString() + ".pcmd";
+        float[] ret = new float[DATA_PER_HOUR];
+        string path = "../data/h" + index.ToString() + ".pcmd";
 
         FileStream fstream = File.Open(path, FileMode.Open);
         BinaryReader reader = new BinaryReader(fstream, Encoding.UTF8, false);
         
-        for (int i = 0; i < DATAPOINTS_PER_FILE; i++)
+        for (int i = 0; i < DATA_PER_HOUR; i++)
         { 
             ret[i] = reader.ReadSingle();
         } 
@@ -65,4 +139,83 @@ static class PCM_Frontend
 
         return ret;
     }
+
+    public static Button ButtonClicked(Button[] btns, Vector2 mousePos)
+    {
+        for (int i = 0; i < btns.Length; i++)
+        {
+            if (mousePos.X > btns[i].X && mousePos.X < btns[i].X + btns[i].Width && 
+                mousePos.Y > btns[i].Y && mousePos.Y < btns[i].Y + btns[i].Height)
+            {
+                return btns[i];
+            }
+        }
+        return Button.btn_null;
+    }
+
+    public static Button[] SpawnButtons(string[] buttonPresets, int y)
+    {
+        Button[] btns = new Button[buttonPresets.Length];
+        int offset = 5;
+        int i = 0;
+        foreach (string str in buttonPresets)
+        {
+            btns[i] = new Button(str, offset, y, 20, textColor, buttonColor);
+            offset += btns[i].Width + 5;
+            i++;
+        }
+        return btns;
+    }
+
+    public static void DrawGrid(int lines)
+    {
+        lines++;
+        int yFraction = HEIGHT / lines;
+        for (int y = 0; y < lines; y++)
+        {
+            Raylib.DrawLine(0, yFraction * y, WIDTH, yFraction * y, gridColor);
+            string percText = (100 - (float)y / lines * 100).ToString() + "%";
+            Raylib.DrawText(percText, 5, yFraction * y, 20, textColor);
+        }
+
+        int xFraction = WIDTH / lines;
+        for (int x = 0; x < lines; x++)
+        {
+            Raylib.DrawLine(xFraction * x, 0, xFraction * x, HEIGHT, gridColor);
+        }
+        return;
+    }
+}
+
+public class Button
+{
+	public string Text;
+	public int X;
+	public int Y;
+	public int Height;
+	public int Width;
+	public Color BGColor;
+	public Color FGColor;
+
+	public int TextSize;
+
+    public static Button btn_null = null; 
+
+	public Button(string text, int x, int y, int size, Color fg, Color bg)
+	{
+		Text = text;
+		X = x;
+		Y = y;
+		FGColor = fg;
+		BGColor = bg;
+        TextSize = size;
+		Height = TextSize + 10;
+		Width = Raylib.MeasureText(Text, TextSize) + 10;
+		
+	}
+	public void Draw()
+	{
+		Raylib.DrawRectangle(X, Y, Width, Height, BGColor);
+		Raylib.DrawText(Text, X + 5, Y + 5, TextSize, FGColor);
+	}
 }
